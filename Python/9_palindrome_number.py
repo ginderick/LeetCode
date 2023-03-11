e7422209e2c69ab75bac1a5d92554389c2838f5a
Gin Derick <ginderick@pop-os.localdomain>
2023-03-11 11:50:44 +0800

    update problems and solutions

---
+++ Python/9_palindrome_number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+
+class Solution_2:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x > 0 and x % 10 == 0):
+            return False
+
+        return x == self.reverseUtil(x)
+
+    def reverseUtil(self, x: int):
+        result = 0
+
+        while x != 0:
+            digit = x % 10
+            result = result * 10 + digit
+            x = int(x / 10)
+
+        return result
